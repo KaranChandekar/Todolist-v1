c61a1f4d8ce72eaf5f5194dfb48521f2f8fb1ec7
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-04 22:18:20 +0530

    add app.listen

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function () {
-  res.send("Server started on port 3000");
+app.get("/", function (req, res) {
+  res.send("Hello");
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
 });
