b39e4e40fd7ff4f3a67065485d46b0cfa47296c1
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-04 22:16:18 +0530

    add app.get

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+
+app.get("/", function () {
+  res.send("Server started on port 3000");
+});
