ffb3275b30e0b08424c6c6dca195ce105796dff8
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-05 17:25:32 +0530

    add res.sendFile insted of res.send

--- app.js
+++ app.js
@@ -6,19 +6,15 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.get("/", function (req, res) {
-
   var today = new Date();
   var currentDay = today.getDay();
 
   if (currentDay === 6 || currentDay === 0) {
-    res.write("<h1>Yey! it's the weekend!</h1>");
-    res.write("<p>Today is saturday!</p>");
+    res.sendFile(__dirname + "/weekend.html");
   } else {
     res.write("<h1>Boo! I have to work!</h1>");
     res.write("<p>It is not the weekend!</p>");
   }
-  res.send();
-
 });
 
 app.listen(3000, function () {
