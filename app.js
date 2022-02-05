8afdefef6d47e8397639c02bd1d51eb105453b0c
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-05 17:17:05 +0530

    revise res.send

--- app.js
+++ app.js
@@ -6,7 +6,19 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Hello");
+
+  var today = new Date();
+  var currentDay = today.getDay();
+
+  if (currentDay === 6 || currentDay === 0) {
+    res.write("<h1>Yey! it's the weekend!</h1>");
+    res.write("<p>Today is saturday!</p>");
+  } else {
+    res.write("<h1>Boo! I have to work!</h1>");
+    res.write("<p>It is not the weekend!</p>");
+  }
+  res.send();
+
 });
 
 app.listen(3000, function () {
