461d5e1a8c8750b05f5d1ab2d0d5969995d08fdd
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-05 21:39:50 +0530

    add app.post

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", function (req, res) {
   var today = new Date();
 
@@ -18,6 +20,11 @@ app.get("/", function (req, res) {
   var day = today.toLocaleDateString("en-US", options);
 
   res.render("list", { kindOfDay: day });
+});
+
+app.post("/", function (req, res) {
+  var item = req.body.newItem;
+  console.log(item);
 });
 
 app.listen(3000, function () {
