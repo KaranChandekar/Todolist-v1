40dbd766fb063a60b02a92a02e881cce38769ded
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-05 18:07:32 +0530

    add list.ejs and use it

--- app.js
+++ app.js
@@ -5,17 +5,20 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("view engine", "ejs");
+app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
   var today = new Date();
   var currentDay = today.getDay();
+  var day = "";
 
   if (currentDay === 6 || currentDay === 0) {
-    res.sendFile(__dirname + "/weekend.html");
+    day = "Weekend";
   } else {
-    res.sendFile(__dirname + "/weekday.html");
+    day = "Weekday";
   }
+
+  res.render("list", { kindOfDay: day });
 });
 
 app.listen(3000, function () {
